585eaac2aa69b648b0dca2493d10984f427d04f5
Yujinn <TFisk1987@gmail.com>
2016-02-21 08:54:20 -0700

    Added new character

--- Game/src/game/resources/Launcher.java
+++ Game/src/game/resources/Launcher.java
@@ -13,8 +13,11 @@ public class Launcher {
  public static void main(String[] args) {
   Character Claire = new Character("Claire", "Ellis", true, 15, 10, 45, 40, "Hacking");
   Character Brendon = new Character("Brendon", "Felkins", false, 10, 40, 20, 32, "");
-  Character Emerson = new Character("Emerson", "Marks", false, 40, 25, 10, 30, "");
-    
+  Character Emerson = new Character("Emerson", "Marks", false, 40, 25, 10, 30, "Security Access");
+  Character Erin = new Character("Erin","Pointspear",true,10, 45, 30, 15, ""); 
+  Character Alex = new Character("Alex","Rierden",false,30,30,30,10,"");
+  Character Character6 = new Character("Create","Me",false,10,10,10,10,"");
+  
   MainMenu MainMenu = new MainMenu();//create frame
         MainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MainMenu.setSize(900, 720);//set size of game frame
