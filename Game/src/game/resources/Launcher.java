7cd46ecba46f41d40b91b57768ebb624825f76e4
Howl <Howl@Calcifer.Home>
2016-02-21 12:26:53 -0700

    added jean to character six, updated stats for four other characters. Still need stat changes for Alex and Claire.

--- Game/src/game/resources/Launcher.java
+++ Game/src/game/resources/Launcher.java
@@ -5,12 +5,12 @@ import javax.swing.JFrame;
 public class Launcher {
 
  public static void main(String[] args) {
-  Character Claire = new Character("Claire", "Ellis", true, 15, 10, 45, 40, "Hacking");
-  Character Brendon = new Character("Brendon", "Felkins", false, 10, 40, 20, 32, "");
-  Character Emerson = new Character("Emerson", "Marks", false, 40, 25, 10, 30, "Security Access");
+  Character Claire = new Character("Claire", "Ellis", true, 10, 10, 45, 40, "Hacking");
+  Character Brendon = new Character("Brendon", "Felkins", false, 8, 40, 20, 32, "");
+  Character Emerson = new Character("Emerson", "Marks", false, 40, 18, 12, 30, "Security Access");
   Character Erin = new Character("Erin","Pointspear",true,10, 45, 30, 15, ""); 
   Character Alex = new Character("Alex","Rierden",false,30,30,30,10,"");
-  Character Character6 = new Character("Create","Me",false,10,10,10,10,"");
+  Character Jean = new Character("Jean","Clark",true,36,29,23,12,"Personnel History Access");
   
   MainMenu MainMenu = new MainMenu();//create frame
         MainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
